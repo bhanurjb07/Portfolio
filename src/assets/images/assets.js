8c287d311ef2b7bc9fb902c3445264263a5bf7c2
BHANUPRATAP SINGH <bhanurjb21@gmail.com>
2025-12-15 04:40:23 +0530

    note app added

--- src/assets/images/assets.js
+++ src/assets/images/assets.js
@@ -1,9 +1,11 @@
 import portfolio from './portfolio.png';
 import taxpal from './taxpal.png';
+import noteapp from './noteapp.png';
 
 const assets = {
   portfolio,
   taxpal,
+  noteapp,
 };
 
 export default assets;
